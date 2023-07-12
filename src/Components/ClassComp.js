9b6c85a635947bbc2298d8345f25ca2817669701
adonijah01 <adonijahsingoei@gmail.com>
2023-07-13 00:03:59 +0300

    My-react-app

---
+++ src/Components/ClassComp.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export class ClassComp extends React.Component{
+    render(){
+        return <p> This is a class component</p>
+    }
+}
+export class ClassComp1 extends React.Component{
+    render(){
+        return <p> Hey this is another class</p>
+    }
+}
---
+++ src/Components/FunctionalComp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function FunctionComp(){
+    return <p> This is a functional component</p>
+}
+
+export default FunctionComp;
